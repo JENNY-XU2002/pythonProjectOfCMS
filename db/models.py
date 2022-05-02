3a77fdefa18b6661c9923dc1494dc428de75f1eb
janutwfour <80673120+janutwfour@users.noreply.github.com>
2022-05-02 19:08:18 +0800

    创建学生类，老师类，学校类，课程类，管理员类

---
+++ db/models.py
@@ -0,0 +1,24 @@
+class Student:
+    def __init__(self):
+        pass
+
+
+class Teacher:
+    def __init__(self):
+        pass
+
+
+class School:
+    def __init__(self):
+        pass
+
+
+class Course:
+    def __init__(self):
+        pass
+
+
+class Administrator:
+    def __init__(self):
+        pass
+
